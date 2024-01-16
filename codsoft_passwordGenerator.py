9e31b2e51c04d7c9c800baacd9923b7fb92958f6
pritimithapure <156600913+pritimithapure@users.noreply.github.com>
2024-01-16 22:07:20 +0530

    Add files via upload

---
+++ codsoft_passwordGenerator.py
@@ -0,0 +1,66 @@
+import string
+import random
+
+if __name__=="__main__":
+    s1=string.ascii_lowercase
+    s2=string.ascii_uppercase
+    s3=string.digits
+    s4=string.punctuation
+    try:
+        plen=int(input("Enter password length:"))
+    except:
+        print("You have entered invalid length")
+        plen=int(input("Enter password length:"))
+
+    s=[]
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    random.shuffle(s)
+    print("Your password is: ")
+    print("".join(s[0:plen]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import string
+# import random
+
+
+# s1=string.ascii_lowercase
+# # print(s1)
+# s2=string.ascii_uppercase
+# # print(s2)
+# s3=string.digits
+# # print(s3)
+# s4=string.punctuation
+# # print(s4)
+
+# plen=int(input("Enter password length:"))
+
+# s=[]
+# s.extend(list(s1))
+# s.extend(list(s2))
+# s.extend(list(s3))
+# s.extend(list(s4))
+# # print(s)
+# random.shuffle(s)
+# # print(s)
+# print("Your password is: ")
+# print("".join(s[0:plen]))
